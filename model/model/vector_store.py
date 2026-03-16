1c742088a02cc9bec594defe6f1905508c63e8ad
dunkel06 <ryham.lafri02@gmail.com>
2026-03-16 21:24:49 +0100

    Implemented FAISS vector store

---
+++ model/model/vector_store.py
@@ -0,0 +1,15 @@
+import faiss
+import numpy as np
+
+class VectorStore:
+    def __init__(self, dimension):
+        self.index = faiss.IndexFlatL2(dimension)
+        self.texts = []
+
+    def add(self, embeddings, texts):
+        self.index.add(np.array(embeddings))
+        self.texts.extend(texts)
+
+    def search(self, query_embedding, k=3):
+        distances, indices = self.index.search(query_embedding, k)
+        return [self.texts[i] for i in indices[0]]
